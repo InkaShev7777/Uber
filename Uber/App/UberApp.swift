7cdea24d4be3e72967fd66c5c0dfdb08c4e423f0
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-09-08 14:59:02 +0300

    init application

---
+++ Uber/App/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Ilya Schevchenko on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Uber/Core/View/UberMapViewRepresentable.swift
@@ -0,0 +1,19 @@
+//
+//  UberMapViewRepresentable.swift
+//  Uber
+//
+//  Created by Ilya Schevchenko on 08.09.2024.
+//
+
+import SwiftUI
+import MapKit
+ 
+struct UberMapViewRepresentable: UIViewRepresentable {
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        
+    }
+    
+    
+}
